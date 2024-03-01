9d19b65d3ce99d34dd86ccfbdb949ef4d3afb70b
Felipe marcon <felipemarcon357@gmail.com>
2024-03-01 01:42:10 -0300

    iniciando contruçao da segunda pagina

--- src/app/page1/page.tsx
+++ src/app/page1/page.tsx
@@ -4,8 +4,15 @@
 export default function Loginpage() {
 
     return (
-        <div>
-
+        <div className="h-screen w-screen backdrop-blur-[8px] bg-black/50 flex flex-col justify-center items-center p-8">
+            <div className="bg-black w-5/12 h-5/12 p-4  ">
+                <p>Drama</p>
+                <div></div>
+            </div>
+            <div className="bg-black w-5/12 h-5/12 p-4">
+                <p>Comédia</p>
+                <div></div>
+            </div>
         </div>
     );
 }

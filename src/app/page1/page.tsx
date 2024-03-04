1ce6f4b8d63818f6f291bf8b193aec647b18661f
Felipe marcon <felipemarcon357@gmail.com>
2024-03-04 13:54:43 -0300

    montando layout de filmes

--- src/app/page1/page.tsx
+++ src/app/page1/page.tsx
@@ -1,31 +1,50 @@
-'use client'
-import React, { useEffect, useState } from 'react';
-import MoviesApi from './components/api'; // Importe o componente moviesApi aqui
 
-export default function Page1() {
-    const [movieData, setMovieData] = useState(null);
+import React from 'react';
+import { moviesList } from './helpers/api';
 
-    useEffect(() => {
-        // Não precisa mais chamar MoviesApi() aqui
-    }, []);
+function transformObjectIntoComponent(moviesObject: any, index: number) {
+    return <div key={index} className='flex-shrink-0 mx-2 w-80 p-10 flex flex-col items-center' >
+        <div className='m-auto text-2xl' >{moviesObject.titleText}</div>
+        <div> <img className='h-80 w-80' src={moviesObject.primaryImage as string} /></div>
+
+    </div >
+}
+
+export default async function Page1() {
+    let moviesDrama;
+
+    let moviesAdventure;
+    let moviesAction;
+
+    const moviesL: any = await moviesList()
+    if (moviesL) {
+        moviesDrama = moviesL.Drama.map(transformObjectIntoComponent)
+        moviesAdventure = moviesL.Adventure.map(transformObjectIntoComponent)
+        moviesAction = moviesL.Action.map(transformObjectIntoComponent)
+    } else {
+        moviesDrama = []
+        moviesAdventure = []
+        moviesAction = []
+    }
+
+    console.log(moviesL.Drama)
 
     return (
-        <div className="h-screen w-screen backdrop-blur-[8px] bg-black/50 flex flex-col justify-center items-center p-8 ">
+        <div className="h-[170vh] w-screens backdrop-blur-[8px] bg-black/50 flex flex-col justify-center items-center p-10  ">
             {/* Renderizar o componente MoviesApi */}
-            <MoviesApi />
-            <div className='flex flex-col w-full h-2/5' >
-                <h1 className='justify-start'>Drama</h1>
-                <div>
 
-                </div>
+            <div className='flex flex-col w-full h-2/5' >
+                <h1 className='justify-start font-bold text-3xl	'>Drama</h1>
+                <div className='flex flex-row overflow-x-hidden '>{moviesDrama}</div>
             </div>
             <div className='flex flex-col w-full h-2/5' >
-                <h1 className='justify-start'>Adventure</h1>
-                <div>
-
-                </div>
+                <h1 className='justify-start font-bold text-3xl	'>Adventure</h1>
+                <div className='flex flex-row overflow-x-hidden'>{moviesAdventure}</div>
             </div>
-
+            <div className='flex flex-col w-full h-2/5' >
+                <h1 className='justify-start font-bold text-3xl	'>Adventure</h1>
+                <div className='flex flex-row overflow-x-hidden'>{moviesAction}</div>
+            </div>
 
 
         </div>

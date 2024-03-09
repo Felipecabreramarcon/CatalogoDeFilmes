7b03a15b4a00cd1f103ac1c521a57914f7d440b0
Felipe marcon <felipemarcon357@gmail.com>
2024-03-09 00:17:56 -0300

    Pagina de login responsiva

--- src/app/MoviesPage/components/listaDeFilmes.tsx
+++ src/app/MoviesPage/components/listaDeFilmes.tsx
@@ -48,6 +48,17 @@ export default function listaDeFilmes({ genero }: ListaDeFilmesProps) {
     const lista = data || []
     const listaMapped = lista.map(transformMovieIntoComponent)
 
+    if (isLoading) {
+        return (
+            <div className='flex flex-col w-[90vw] h-2/5 ' >
+                <h1 className=' justify-start font-bold text-3xl mb-4'>{genero}</h1>
+                <div className='w-full h-full flex items-center justify-center'><div className='h-[100px] w-[100px] border-solid opacity-50 border-black border-[12px] rounded-[50%] border-r-rosa animate-[spin_1s_ease_infinite] justify-center items-center'></div></div>
+
+            </div >
+        )
+    }
+
+
 
     let divfilme = <Carousel swipeable={false}
         draggable={false}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,48 +1,39 @@
 'use client'
+import { Field, Form, Formik } from "formik";
 import React from "react";
 import { useState } from "react";
 import * as yup from "yup";
 
+interface MyFormValues {
+    Email: string;
+    Password: string
+}
 
 export default function Loginpage() {
-    const [email, setEmail] = useState('');
-    const [senha, setSenha] = useState('');
     const [message, setMessage] = useState('');
 
 
     //validaçao para email (yup considera 'xxxxx@xxxxxxxx' um email válido.)
     const EMAIL_REGX = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-    //obter valores de email e senha respectivamente
-    const handleChangeEmail = (event: React.ChangeEvent<HTMLInputElement>) => {
-        const { value } = event.currentTarget;
-        setEmail(value);
-    };
-
-    const handleChangeSenha = (event: React.ChangeEvent<HTMLInputElement>) => {
-        const { value } = event.currentTarget;
-        setSenha(value);
-    };
-
     //funçao de redirecionamento
-    const sendMsg = async (e: any) => {
+    const sendMsg = async (values: any) => {
 
-        e.preventDefault()
         //caso ambos os campos estejam vazios apresenta: 'Preencha os campos', caso contrario apresenta mensagem determinada na funcao Validate().
-        if (email.trim() === '' && senha.trim() === '') {
+        if (values.Email == '' && values.Password == '') {
             setMessage('Preencha os campos');
             return;
         }
-        const valid = await Validate();
+        const valid = await Validate(values);
 
         // Se a validação for bem-sucedida, redireciona para a página seguinte
         if (valid) {
-            window.location.href = '/page1'; // Redirecionamento
+            window.location.href = '/MoviesPage'; // Redirecionamento
         }
     };
 
     // Função para validar o formulário
-    async function Validate() {
+    async function Validate(values: any) {
         //Impoe condicoes de validaçao
         let schema = yup.object().shape({
             email: yup.string().matches(EMAIL_REGX, 'Insira um email válido').required('Insira um email!'),
@@ -51,8 +42,8 @@ export default function Loginpage() {
         //Valida as condiçoes impostas acima
         try {
             await schema.validate({
-                email: email,
-                senha: senha
+                email: values.Email,
+                senha: values.Password
             });
             //caso as condicoes sejam satisfeitas:
             setMessage('')
@@ -64,26 +55,35 @@ export default function Loginpage() {
             return false
         }
     }
+
+    const initialValues: MyFormValues = { Email: '', Password: '' };
+
     //conteudo da página
     return (
-        <div className="flex flex-row h-screen">
+        <div className="flex desktop:flex-row laptop:flex-row  mobile:flex-col  h-screen mobile:justify-center mobile:items-center p-8">
 
-            <div className="w-2/3 flex flex-col flex-wrap justify-center items-start px-32 gap-12">
-                <h1 className="font-bold w-2/5 text-4xl">Bem Vindo ao Sistema de XPTO</h1>
-                <h3 className="text-2xl leading-7">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</h3>
+            <div className="w-2/3 mobile:w-full flex flex-col justify-center mobile:items-center desktop:items-start laptop:items-start px-32 gap-12">
+                <h1 className="font-bold mobile:w-96 mobile:text-2xl tablet:text-4xl laptop:text-4xl desktop:text-4xl laptop:text-start desktop:text-start mobile:text-center">Bem Vindo ao Sistema de XPTO</h1>
+                <h3 className="desktop:text-start desktop:text-2xl laptop:w-full laptop:text-2xl desktop:w-2/3 laptop:text-start leading-7 mobile:w-96 mobile:text-base mobile:text-center">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</h3>
             </div>
-            <div className="w-1/3 bg-opacity-25 border-solid border border-white bg-white rounded-2xl m-10 flex flex-col justify-center items-center p-8 gap-8 backdrop-blur-sm" style={{ borderWidth: '0.5px' }}>
+            <div className="desktop:w-2/5 tablet:w-2/3 laptop:h-full desktop:h-full mobile:h-1/2 mobile:w-full bg-opacity-25 border-solid border border-white bg-white rounded-2xl m-10 flex flex-col justify-center items-center p-8 gap-8 backdrop-blur-sm" style={{ borderWidth: '0.5px' }}>
                 <h1 className="text-center font-bold text-4xl whitespace-nowrap">Login</h1>
-                <div className="w-full flex flex-col items-center gap-4">
-                    <input className="w-full bg-transparent p-4 border-2 border-rosa rounded placeholder:text-white shadow-[0_4px_4px_0_rgba(0,0,0,0.25)] focus:outline-none input" value={email} onChange={handleChangeEmail} type="email" placeholder="Email" ></input>
+                <Formik
+                    initialValues={initialValues}
+                    onSubmit={values => {
+                        sendMsg(values)
+                    }} >
+                    <Form className="w-full flex flex-col items-center gap-4">
+                        <Field autocomplete='off' className="w-full bg-transparent p-4 border-2 border-rosa rounded placeholder:text-white shadow-[0_4px_4px_0_rgba(0,0,0,0.25)] focus:outline-none input" name='Email' type="email" placeholder="Email" ></Field>
 
-                    <input className="w-full bg-transparent p-4 border-2 border-rosa rounded  placeholder:text-white shadow-[0_4px_4px_0_rgba(0,0,0,0.25)] focus:outline-none input" value={senha} onChange={handleChangeSenha} type="password" placeholder="Senha"></input>
+                        <Field className="w-full bg-transparent p-4 border-2 border-rosa rounded  placeholder:text-white shadow-[0_4px_4px_0_rgba(0,0,0,0.25)] focus:outline-none input" name="Password" type="password" placeholder="Senha"></Field>
 
 
-                    <button className="bg-rosa w-full flex justify-center p-4 rounded shadow-[0_4px_4px_0_rgba(0,0,0,0.25)] focus:outline-none focus:border-none" onClick={sendMsg}>Entrar</button>
-                    <div className="h-4 font-bold text-red-600">{message}</div>
+                        <button type="submit" className="bg-rosa w-full flex justify-center p-4 rounded shadow-[0_4px_4px_0_rgba(0,0,0,0.25)] focus:outline-none focus:border-none" >Entrar</button>
+                        <div className="h-4 font-bold text-red-600">{message}</div>
 
-                </div>
+                    </Form>
+                </Formik>
             </div>
 
         </div >
